f761cb44ef8d797af0b51943180e06edb9989ca7
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-06-17 22:23:20 +0530

    Create TrueValue.py

---
+++ KissFlow/TrueValue.py
@@ -0,0 +1,14 @@
+"""
+Output:
+One value here
+
+"""
+d = {True: "True value here", None: "None value here"}
+
+d[1] "One value here"
+
+d[2] "Two value here"
+
+d[None]= "None value here again"
+
+print(d[True])
