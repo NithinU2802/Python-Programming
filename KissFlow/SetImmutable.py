dd91cfdf95327ef227e8329f425afe08e0d9f01a
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-06-18 08:32:49 +0530

    Create SetImmutable.py

---
+++ KissFlow/SetImmutable.py
@@ -0,0 +1,14 @@
+'''
+
+Set is immutable....!
+
+<---immutable an unchangable one got an error--->
+
+'''
+
+a={0,1,2}
+b={2,3,4}
+
+print(len(a+b))
+
+# print(len(list(a)+list(b))) will work instead...!
